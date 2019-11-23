0b9f3d5e78166ef64981d1ad61849c9dbe65cb1c
Gabe <finkegabriel@gmail.com>
2019-11-22 19:49:11 -0700

    adding new main and README

---
+++ README.md
@@ -0,0 +1,10 @@
+RUSTY 
+
+A rust robotics "library/framework" in it's infancy
+
+- tested on raspberry pi zero
+
+referenced this link for cross compiling to the pi zero
+
+https://rust.azdevs.org/2019-07-24/
+
--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,14 @@ const GPIO_MotorTwo: u8 =24; //???
 fn main() -> Result<(), Box<dyn Error>> {
     // Retrieve the GPIO pin and configure it as an output.
     let mut apin = Gpio::new()?.get(GPIO_Motor)?.into_output();
-    // let mut bpin = Gpio::new()?.get(GPIO_MotorTwo)?.into_output();
-    // print!("running motors!!");
+    let mut bpin = Gpio::new()?.get(GPIO_MotorTwo)?.into_output();
+    
+    print!("running motors!!");
+
     loop{
         apin.toggle();
+        thread::sleep(Duration::from_millis(500));
+        bpin.toggle();
         thread::sleep(Duration::from_millis(500));
     }
 }
