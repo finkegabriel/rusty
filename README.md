64431e3d95b4818c5173abcc89ba8dc380b27bab
Gabe <finkegabriel@gmail.com>
2019-11-23 01:00:11 -0700

    no loop example

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ referenced this link for cross compiling to the pi zero
 
 https://rust.azdevs.org/2019-07-24/
 
+commands to build && transfer binary
+
+cargo build --target=arm-unknown-linux-gnueabihf
+
+scp target/arm-unknown-linux-gnueabihf/debug/rusty pi@raspberrypi.local:~/ 
+
+created alias for linux 
+
+`alias rusty='cd ~/rusty && cargo build --target=arm-unknown-linux-gnueabihf && scp target/arm-unknown-linux-gnueabihf/debug/rusty pi@raspberrypi.local:~/`
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use std::error::Error;
 use std::thread;
 use std::time::Duration;
-
 use rppal::gpio::Gpio;
+// use lib::bcm;
 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
 const GPIO_Motor: u8 = 23;
-const GPIO_MotorTwo: u8 =24; //???
+const GPIO_MotorTwo: u8 = 24; //???
+
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Retrieve the GPIO pin and configure it as an output.
     let mut apin = Gpio::new()?.get(GPIO_Motor)?.into_output();
     let mut bpin = Gpio::new()?.get(GPIO_MotorTwo)?.into_output();
-    
-    print!("running motors!!");
 
-    loop{
+    
+        println!("running motors!!");
         apin.toggle();
-        thread::sleep(Duration::from_millis(500));
-        bpin.toggle();
-        thread::sleep(Duration::from_millis(500));
-    }
+        thread::sleep(Duration::from_millis(1000));
+        apin.set_low();
+        Ok(())
+    
 }
