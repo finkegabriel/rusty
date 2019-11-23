ffa622e5ae17e2a9283c7a1f36a9a0d734db46a4
Gabe <finkegabriel@gmail.com>
2019-11-22 18:32:12 -0700

    init wip

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+
+// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
+const GPIO_Motor: u8 = 23;
+const GPIO_MotorTwo: u8 =24; //???
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Retrieve the GPIO pin and configure it as an output.
+    let mut apin = Gpio::new()?.get(GPIO_Motor)?.into_output();
+    // let mut bpin = Gpio::new()?.get(GPIO_MotorTwo)?.into_output();
+    // print!("running motors!!");
+    loop{
+        apin.toggle();
+        thread::sleep(Duration::from_millis(500));
+    }
+}
